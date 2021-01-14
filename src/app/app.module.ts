5f50cb373e5a3de649471f1e3582ac75757c77f2
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2021-01-14 18:59:06 -0500

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,9 @@ import { CategoriasComponent } from './componentes/categorias/categorias.compone
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { IntroComponent } from './componentes/intro/intro.component';
-import { ContadorpageComponent } from './componentes/contadorPage/contadorpage.component';
-import { InformaticopageComponent } from './componentes/informaticoPage/informaticopage.component';
-import { ElectronicoPageComponent } from './componentes/electronico-page/electronico-page.component';
-import { CurriculumPageComponent } from './componentes/curriculum-page/curriculum-page.component';
-import { FundacionPageComponent } from './componentes/fundacion-page/fundacion-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './componentes/home/home.component';
-import { CertificadoTecnologoContabilidadyFinanzasComponent } from './componentes/certificado-tecnologo-contabilidady-finanzas/certificado-tecnologo-contabilidady-finanzas.component';
 import { FacebookModule } from "ngx-facebook";
-import { CertificadoTecnicoContabilidadFinanzasComponent } from './componentes/certificado-tecnico-contabilidad-finanzas/certificado-tecnico-contabilidad-finanzas.component';
-import { CertificadosCursosContablesComponent } from './componentes/certificados-cursos-contables/certificados-cursos-contables.component';
-import { CertificadoCursoTablasExcelComponent } from './componentes/certificado-curso-tablas-excel/certificado-curso-tablas-excel.component';
-import { CertificadoCursoExcelBasicoComponent } from './componentes/certificado-curso-excel-basico/certificado-curso-excel-basico.component';
-import { CertificadoCursoHerramientasOfficeComponent } from './componentes/certificado-curso-herramientas-office/certificado-curso-herramientas-office.component';
-import { CertificadoCursoNormasOrtograficasEmpresarialesComponent } from './componentes/certificado-curso-normas-ortograficas-empresariales/certificado-curso-normas-ortograficas-empresariales.component';
 
 @NgModule({
   declarations: [
@@ -30,19 +18,7 @@ import { CertificadoCursoNormasOrtograficasEmpresarialesComponent } from './comp
     HeaderComponent,
     FooterComponent,
     IntroComponent,
-    ContadorpageComponent,
-    InformaticopageComponent,
-    ElectronicoPageComponent,
-    CurriculumPageComponent,
-    FundacionPageComponent,
     HomeComponent,
-    CertificadoTecnologoContabilidadyFinanzasComponent,
-    CertificadoTecnicoContabilidadFinanzasComponent,
-    CertificadosCursosContablesComponent,
-    CertificadoCursoTablasExcelComponent,
-    CertificadoCursoExcelBasicoComponent,
-    CertificadoCursoHerramientasOfficeComponent,
-    CertificadoCursoNormasOrtograficasEmpresarialesComponent
   ],
   imports: [
     BrowserModule,
