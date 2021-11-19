9a2c5fe88d0b0b7540dc1250d6739814dc9a672d
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2021-11-19 10:56:20 -0500

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
-import { CategoriasComponent } from './componentes/categorias/categorias.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { IntroComponent } from './componentes/intro/intro.component';
@@ -11,9 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './componentes/home/home.component';
 import { FacebookModule } from "ngx-facebook";
 import { CarouselComponent } from './componentes/carousel/carousel.component';
-import { IngSistemasComponent } from './componentes/ing-sistemas/ing-sistemas.component';
 import { CursosDestacadosComponent } from './componentes/cursos-destacados/cursos-destacados.component';
-import { ProfesoradoComponent } from './componentes/profesorado/profesorado.component';
 import { DiferenciasMercadoComponent } from './componentes/diferencias-mercado/diferencias-mercado.component';
 import { GuiaProcedimientosComponent } from './componentes/guia-procedimientos/guia-procedimientos.component';
 import { IntegrantesGiideaComponent } from './componentes/integrantes-giidea/integrantes-giidea.component';
@@ -21,15 +18,12 @@ import { IntegrantesGiideaComponent } from './componentes/integrantes-giidea/int
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriasComponent,
     HeaderComponent,
     FooterComponent,
     IntroComponent,
     HomeComponent,
     CarouselComponent,
-    IngSistemasComponent,
     CursosDestacadosComponent,
-    ProfesoradoComponent,
     DiferenciasMercadoComponent,
     GuiaProcedimientosComponent,
     IntegrantesGiideaComponent,
